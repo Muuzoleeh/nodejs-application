4273085f049cefdab79483ee01cddafb37c8cff8
Muuzoleeh <113614568+Muuzoleeh@users.noreply.github.com>
2023-05-28 10:49:40 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': 'd8ad1d43729c034ff62ac50f3564bea5a1c6e7cb',
+	    'sonar.login': '703fabac54cf843cebddf9806f5eb8b617913ee2',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
